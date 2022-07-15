2fb8ce528c267972807447344c9996492aca0b54
Vaios Anastasiadis <vaios.anastasiadis@gfk.com>
2022-07-15 11:11:29 +0300

    Prepare project for sass and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import 'styles/global.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Game } from 'components';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Game />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
---
+++ src/components/Game.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Game = () => (
+  <div className="game">
+    <div className="board">Board</div>
+  </div>
+);
+
+export default Game;
